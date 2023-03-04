56b602cfba04a096fd259a5e8c4d8175c4baf07f
ASayidov <abdullajonsayidov@gmail.com>
2023-03-04 17:09:50 +0500

    added mobile navigation

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const nav = document.getElementById("nav")
+const navImg = document.getElementById("nav-btn-img")
+
+
+const navchange = () => {
+  if (nav.classList.toggle("open")) {
+    navImg.src = "./img/icons/nav-close.svg"
+
+  } else {
+    navImg.src = "./img/icons/nav-open.svg"
+  }
+}
